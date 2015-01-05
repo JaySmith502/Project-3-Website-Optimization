bdffe53a31b5c6c50eaaaf1a6459e7fa056866f6
Jay Smith <smith1jason@yahoo.com>
2015-01-05 13:29:08 -0500

    Updated main.js
    Took first For loop from 100 to 50 since on my large screen the max
    number of pizzas that can display are 40, left 10 in there in case of
    larger screen.  Updated Second (.mover) For loop as same.

--- views/js/main.js
+++ views/js/main.js
@@ -465,7 +465,7 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
-for (var i = 2; i < 100; i++) {
+for (var i = 2; i < 50; i++) {
   var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
@@ -499,7 +499,7 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
+  for (var i = 0; i < 50; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
