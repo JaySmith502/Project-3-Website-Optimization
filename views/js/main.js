4c208acea1bdea724dbf9eefdd503c512a624cc6
Jay Smith <smith1jason@yahoo.com>
2015-01-05 13:43:48 -0500

    update main.js
    took both For loop max numbers down to 40 to see if it has an
    appreciable effect on event rendering.

--- views/js/main.js
+++ views/js/main.js
@@ -465,7 +465,7 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
-for (var i = 2; i < 50; i++) {
+for (var i = 2; i < 40; i++) {
   var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
@@ -499,7 +499,7 @@ function updatePositions() {
   var action = document.body.scrollTop;
   window.performance.mark("mark_start_frame");
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < 50; i++) {
+  for (var i = 0; i < 40; i++) {
     var phase = Math.sin((action / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
