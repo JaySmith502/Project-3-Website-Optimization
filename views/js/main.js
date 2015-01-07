6b5918e429309c8dd7692d427a458f8f041ff0ee
Jay Smith <smith1jason@yahoo.com>
2015-01-07 13:52:11 -0500

    update main.js
    put two var's back in generator function, slider still doesn't work

--- views/js/main.js
+++ views/js/main.js
@@ -278,7 +278,6 @@ function getNoun(y) {
   }; 
 };
 //appears to be some redundancy between generator and randomName functions, going to try and pull out some of this code so it's not pulling every time.
-
 var adjectives = ["dark", "color", "whimsical", "shiny", "noise", "apocalyptic", "insulting", "praise", "scientific"];  // types of adjectives for pizza titles
 var lengthAdj = adjectives.length;
 var nouns = ["animals", "everyday", "fantasy", "gross", "horror", "jewelry", "places", "scifi"];                        // types of nouns for pizza titles
@@ -290,6 +289,8 @@ var randomNoun = parseInt(Math.random() * nouns.length);
 function generator(adj, noun) {
   var adjectives = getAdj(adj);
   var nouns = getNoun(noun);
+  var randomAdjective = parseInt(Math.random() * lengthAdj);
+  var randomNoun = parseInt(Math.random() * lengthNoun);
   var name = "The " + adjectives[randomAdjective].capitalize() + " " + nouns[randomNoun].capitalize();
   return name;
 };
