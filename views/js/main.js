4000d9fbda293ddb2b3c9191c966bd5bff8d4608
Jay Smith <smith1jason@yahoo.com>
2015-01-07 13:47:43 -0500

    update main.js
    removed i and s variables from update for loop at bottom of code to
    reduce iterations back up to variable.s

--- views/js/main.js
+++ views/js/main.js
@@ -531,16 +531,15 @@ window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-  var cols = 8;
-  var s = 256;
+
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
-    elem.basicLeft = (i % cols) * s;
-    elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    elem.basicLeft = (i % cols) * 256;
+    elem.style.top = (Math.floor(i / 8) * 256) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
