0f2ae90c22ffddcee4db1570c85e4e596739c7b5
smith1jason <smith1jason@users.noreply.github.com>
2015-01-07 19:50:54 -0500

    update main.js
    Moved elem array out of for look on line 548

--- views/js/main.js
+++ views/js/main.js
@@ -538,7 +538,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   var move = document.querySelector("#movingPizzas1");
-  for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
@@ -546,6 +545,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.style.left = (i % cols) * s + 'px';
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
+  for (var i = 0; i < 200; i++) {
     move.appendChild(elem);
   }
   updatePositions();
