379ff2fb746b84cb6b8bcc2f93a8e7d563836043
Jay Smith <smith1jason@yahoo.com>
2015-01-09 19:23:32 -0500

    update main.js
    put var randoms back

--- views/js/main.js
+++ views/js/main.js
@@ -279,13 +279,13 @@ var lengthAdj = adjectives.length;
 var nouns = ["animals", "everyday", "fantasy", "gross", "horror", "jewelry", "places", "scifi"];                        // types of nouns for pizza titles
 var lengthNoun = nouns.length;
 
-
 // Generates random numbers for getAdj and getNoun functions and returns a new pizza name
 // Removed unnecessary semicolon
 function generator(adj, noun) {
   var adjectives = getAdj(adj);
   var nouns = getNoun(noun);
-
+  var randomAdjective = parseInt(Math.random()*lengthAdj, 10);
+  var randomNoun = parseInt(Math.random() * lengthNoun, 10);
   var name = "The " + adjectives[randomAdjective].capitalize() + " " + nouns[randomNoun].capitalize();
   return name;
 }
