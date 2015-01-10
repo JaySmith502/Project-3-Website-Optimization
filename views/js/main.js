b2fbaca1a3df30d0186baf1c36fbc785fde4bb18
Jay Smith <smith1jason@yahoo.com>
2015-01-10 09:02:30 -0500

    Revert "update main.js"
    This reverts commit 6b3fabfbd059bcdd3277140512fff1d52152edf3.
    
    Conflicts:
    	views/js/main.js

--- views/js/main.js
+++ views/js/main.js
@@ -528,6 +528,7 @@ function updatePositions() {
 window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
+<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
@@ -539,6 +540,21 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
+=======
+//removd 5 elem var calls from for loop to avoid calling it every time when it only needs once.
+document.addEventListener('DOMContentLoaded', function() {
+  var cols = 8;
+  var s = 256;
+  var move = document.querySelector("#movingPizzas1");   
+  var elem = document.createElement('img');
+  elem.prototype.className = 'mover';
+  elem.prototype.src = "images/pizza.png";
+  elem.prototype.style.height = "100px";
+  elem.prototype.style.width = "73.333px";
+
+  for (var i = 0; i < 33; i++) {
+
+>>>>>>> parent of 6b3fabf... update main.js
     elem.style.left = (i % cols) * s + 'px';
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     move.appendChild(elem);
