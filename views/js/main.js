dcbf08161162d8934ba64dee0f44d2690e6d1249
Jay Smith <smith1jason@yahoo.com>
2015-01-11 14:44:01 -0500

    update main..js
    reverted adjectives and nouns to global variables, too many calls to
    them in randomName and rest of naming functions

--- views/js/main.js
+++ views/js/main.js
@@ -286,10 +286,12 @@ function getNoun(y) {
 
 // Generates random numbers for getAdj and getNoun functions and returns a new pizza name
 // added decimal 10 to parseInt per Google Developer Style requirement found in Audit of site
-// moved adjectives and length vars back into generator f(x) per info read on webreference.com about local vs global variables
-function generator(adj, noun) {
+
   var adjectives = ["dark", "color", "whimsical", "shiny", "noise", "apocalyptic", "insulting", "praise", "scientific"];  // types of adjectives for pizza titles
   var nouns = ["animals", "everyday", "fantasy", "gross", "horror", "jewelry", "places", "scifi"];                        // types of nouns for pizza titles
+
+function generator(adj, noun) {
+
   var adjectives = getAdj(adj);
   var nouns = getNoun(noun);
   var randomAdjective = parseInt(Math.random()* 9, 10);
