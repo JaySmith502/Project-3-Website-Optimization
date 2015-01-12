260d5ff5f4db018d049481c420f58fc0c237fe19
Jay Smith <smith1jason@yahoo.com>
2015-01-12 13:43:53 -0500

    update main.js
    Tried var docs again, this time with global(local) var query above
    switch within changeSliderLabel

--- views/js/main.js
+++ views/js/main.js
@@ -427,16 +427,19 @@ var resizePizzas = function(size) {
   // Changes the value for the size of the pizza above the slider
   //took out document.querySelector command, didn't seem to negatively impact the js function on the page
   function changeSliderLabel(size) {
-    var docs = document;
+    var docs = document.querySelector("#pizzaSize").innerHTML;
     switch(size) {
       case "1":
-        doc.querySelector("#pizzaSize").innerHTML = "Small";
+        //docs.querySelector("#pizzaSize").innerHTML = "Small";
+        docs = "Small";
         return;
       case "2":
-        doc.querySelector("#pizzaSize").innerHTML = "Medium";
+        //docs.querySelector("#pizzaSize").innerHTML = "Medium";
+        docs = "Medium";
         return;
       case "3":
-        doc.querySelector("#pizzaSize").innerHTML = "Large";
+        //docs.querySelector("#pizzaSize").innerHTML = "Large";
+        docs = "Large";
         return;
       default:
         console.log("bug in changeSliderLabel");
