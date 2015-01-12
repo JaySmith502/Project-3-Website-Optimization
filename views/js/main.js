178304704826549f2546e5443e28d2e8f408b26d
Jay Smith <smith1jason@yahoo.com>
2015-01-12 13:44:35 -0500

    Revert "update main.js"
    This reverts commit 260d5ff5f4db018d049481c420f58fc0c237fe19.

--- views/js/main.js
+++ views/js/main.js
@@ -427,19 +427,16 @@ var resizePizzas = function(size) {
   // Changes the value for the size of the pizza above the slider
   //took out document.querySelector command, didn't seem to negatively impact the js function on the page
   function changeSliderLabel(size) {
-    var docs = document.querySelector("#pizzaSize").innerHTML;
+    var docs = document;
     switch(size) {
       case "1":
-        //docs.querySelector("#pizzaSize").innerHTML = "Small";
-        docs = "Small";
+        doc.querySelector("#pizzaSize").innerHTML = "Small";
         return;
       case "2":
-        //docs.querySelector("#pizzaSize").innerHTML = "Medium";
-        docs = "Medium";
+        doc.querySelector("#pizzaSize").innerHTML = "Medium";
         return;
       case "3":
-        //docs.querySelector("#pizzaSize").innerHTML = "Large";
-        docs = "Large";
+        doc.querySelector("#pizzaSize").innerHTML = "Large";
         return;
       default:
         console.log("bug in changeSliderLabel");
