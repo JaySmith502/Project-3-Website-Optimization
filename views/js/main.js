790dfd178f7c19c480dd6d59c9fc474c9091223a
Jay Smith <smith1jason@yahoo.com>
2015-01-14 07:38:21 -0500

    update main.js
    readded height and width attributes

--- views/js/main.js
+++ views/js/main.js
@@ -563,8 +563,8 @@ function createStartPizzas() {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
-//    elem.style.height = "100px";
-//    elem.style.width = "73.333px";
+    elem.style.height = "100px";
+    elem.style.width = "73.333px";
     elem.style.left = (i % cols) * s + 'px';
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     move.appendChild(elem);
