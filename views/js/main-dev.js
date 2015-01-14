0ad01fd893655699b13e77af9d973cc7054d3539
Jay Smith <smith1jason@yahoo.com>
2015-01-14 14:05:47 -0500

    update main-dev.js
    took four elem.var calls out of for loop and put in local

--- views/js/main-dev.js
+++ views/js/main-dev.js
@@ -571,15 +571,16 @@ function createStartPizzas() {
   var s = 256;
   var move = document.querySelector("#movingPizzas1");
 // moved height and width to style tags on pizza.html, not needed to call from javascript
+  var elem = document.createElement('img');
+  elem.className = 'mover';
+  elem.src = "images/pizza-tiny.png";
+  elem.style.height = "100px";
+  elem.style.width = "73.333px";
+
   for (var i = 0; i < 35; i++) {
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza-tiny.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
-    elem.style.left = (i % cols) * s + 'px';
-    elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    move.appendChild(elem);
+  elem.style.left = (i % cols) * s + 'px';
+  elem.style.top = (Math.floor(i / cols) * s) + 'px';
+  move.appendChild(elem);
   }
 //  updatePositions();
 }
