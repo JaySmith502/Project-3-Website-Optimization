8754ec995b55c6144753532397579b33733b785d
Jay Smith <smith1jason@yahoo.com>
2015-01-14 06:46:01 -0500

    update main.js
    put back original f(x) on update Positions to see

--- views/js/main.js
+++ views/js/main.js
@@ -528,6 +528,7 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // updated for loop to remove extraneous e.length function and replaced with 32 max pizzas for [i] since screen can only display 32
 // took (document.body.scrollTop) out of for Loop to remove constant reiteration since it's only necessary to call once and can be recalled as a variable
 // Used translateX function found on piazza forums and in Google research
+/*
 function updatePositions() {
   frame++;
   var action = document.body.scrollTop;
@@ -548,7 +549,8 @@ function updatePositions() {
     logAverageFrame(timesToUpdatePosition);
   }
 }
-
+*/
+/*
 // runs updatePositions on scroll
 window.addEventListener('scroll', updatePositions);
 
@@ -578,3 +580,43 @@ else {
   document.addEventListener("DOMContentLoaded", createStartPizzas());
 }
 
+*/
+function updatePositions() {
+  frame++;
+  window.performance.mark("mark_start_frame");
+
+  var items = document.querySelectorAll('.mover');
+  for (var i = 0; i < items.length; i++) {
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  }
+
+  // User Timing API to the rescue again. Seriously, it's worth learning.
+  // Super easy to create custom metrics.
+  window.performance.mark("mark_end_frame");
+  window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
+  if (frame % 10 === 0) {
+    var timesToUpdatePosition = window.performance.getEntriesByName("measure_frame_duration");
+    logAverageFrame(timesToUpdatePosition);
+  }
+}
+
+// runs updatePositions on scroll
+window.addEventListener('scroll', updatePositions);
+
+// Generates the sliding pizzas when the page loads.
+document.addEventListener('DOMContentLoaded', function() {
+  var cols = 8;
+  var s = 256;
+  for (var i = 0; i < 200; i++) {
+    var elem = document.createElement('img');
+    elem.className = 'mover';
+    elem.src = "images/pizza.png";
+    elem.style.height = "100px";
+    elem.style.width = "73.333px";
+    elem.basicLeft = (i % cols) * s;
+    elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    document.querySelector("#movingPizzas1").appendChild(elem);
+  }
+  updatePositions();
+});
