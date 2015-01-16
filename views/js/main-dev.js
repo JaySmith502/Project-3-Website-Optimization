25804d04126034dd25139d9a3bc3b4e47b749c56
Jay Smith <smith1jason@yahoo.com>
2015-01-16 06:25:47 -0500

    update main-dev.js
    revert

--- views/js/main-dev.js
+++ views/js/main-dev.js
@@ -361,11 +361,11 @@ var resizePizzas = function(size) {
     function sizeSwitch(size) {
         switch (size) {
             case "1":
-                return .25;
+                return 25;
             case "2":
-                return .3333;
+                return 33.33;
             case "3":
-                return .50;
+                return 50;
             default:
                 console.log("bug in sizeSwitcher");
         }
@@ -375,12 +375,10 @@ var resizePizzas = function(size) {
     // took pizzaLength callback out of the equation by assigning 101 (max 100 pizzas)
     // took document.querySelectorAll(".randomPizzaContainer") out of changePizzaSizes f(x)
     function changePizzaSizes(size) {
-        var randomPizzaContainer = document.querySelectorAll(".randomPizzaContainer");
-        var pizzaStandardWidth = randomPizzaContainer[0].style.width;
-        var randomPizzaContainerLength = randomPizzaContainer.length;
+
         var pizza = document.querySelectorAll(".randomPizzaContainer");
         //var pizzaLength = pizza.length;
-        var newSize = sizeSwitch(size);// + "%";
+        var newSize = sizeSwitch(size) + "%";
         console.log(pizzaStandardWidth);
         for (var i = 0; i < 101; i++) {
             pizza[i].style.width = newSize;
